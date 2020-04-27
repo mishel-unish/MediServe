e7a5f83f45a35e6cb4756047a9a7bf0e5ea6a6e3
Mishel <mishel.unish@gmail.com>
2020-04-27 14:26:46 +0600

    Add login page

--- index.php
+++ index.php
@@ -1,63 +1,165 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1, shrink-to-fit=no"
+		/>
+		<title>Login</title>
 
-	<?php include './shared/head.html'; ?>
+		<!-- Prevent the demo from appearing in search engines -->
+		<meta name="robots" content="noindex" />
 
-	<body class="layout-default">
-		<div class="preloader"></div>
+		<!-- Perfect Scrollbar -->
+		<link
+			type="text/css"
+			href="assets/vendor/perfect-scrollbar.css"
+			rel="stylesheet"
+		/>
 
-		<!-- Header Layout -->
-		<div class="mdk-header-layout js-mdk-header-layout">
-			<!-- Header -->
-			<?php include './shared/header.html'; ?>
-			<!-- // END Header -->
+		<!-- App CSS -->
+		<link type="text/css" href="assets/css/app.css" rel="stylesheet" />
+		<link type="text/css" href="assets/css/app.rtl.css" rel="stylesheet" />
 
-			<!-- Header Layout Content -->
-			<div class="mdk-header-layout__content">
-				<div
-					class="mdk-drawer-layout js-mdk-drawer-layout"
-					data-push
-					data-responsive-width="992px"
-				>
-					<div class="mdk-drawer-layout__content page">
-						<div class="container-fluid page__heading-container">
-							<div class="page__heading d-flex align-items-end">
-								<div class="flex">
-									<h1 class="m-0">Dashboard</h1>
-								</div>
+		<!-- Material Design Icons -->
+		<link
+			type="text/css"
+			href="assets/css/vendor-material-icons.css"
+			rel="stylesheet"
+		/>
+		<link
+			type="text/css"
+			href="assets/css/vendor-material-icons.rtl.css"
+			rel="stylesheet"
+		/>
+
+		<!-- Font Awesome FREE Icons -->
+		<link
+			type="text/css"
+			href="assets/css/vendor-fontawesome-free.css"
+			rel="stylesheet"
+		/>
+		<link
+			type="text/css"
+			href="assets/css/vendor-fontawesome-free.rtl.css"
+			rel="stylesheet"
+		/>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script
+			async
+			src="https://www.googletagmanager.com/gtag/js?id=UA-133433427-1"
+		></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag("js", new Date());
+
+			gtag("config", "UA-133433427-1");
+		</script>
+	</head>
+
+	<body class="layout-login">
+		<div class="layout-login__overlay"></div>
+		<div class="layout-login__form bg-white" data-perfect-scrollbar>
+			<div class="d-flex justify-content-center mt-2 mb-5 navbar-light">
+				<a href="index.html" class="navbar-brand" style="min-width: 0;">
+					<!-- <img
+						class="navbar-brand-icon"
+						src="assets/images/stack-logo-blue.svg"
+						width="25"
+						alt="Stack"
+					/> -->
+					<span>MediServe</span>
+				</a>
+			</div>
+
+			<h4 class="m-0">Welcome back!</h4>
+			<p class="mb-5">Login to access your MediServe Account</p>
+
+			<form action="index.html" novalidate>
+				<div class="form-group">
+					<label class="text-label" for="email_2">Email Address:</label>
+					<div class="input-group input-group-merge">
+						<input
+							id="email_2"
+							type="email"
+							required=""
+							class="form-control form-control-prepended"
+							placeholder="john@doe.com"
+						/>
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<span class="far fa-envelope"></span>
 							</div>
 						</div>
-
-						<form class="container-fluid page__container">
-							<div class="card card-form">
-								<div class="row card-form__body card-body">
-
-
-
-								</div>
-							</div>
-
-							<div class="card card-form">
-								<div class="row card-form__body card-body flex justify-content-center">
-
-									<button type="button" class="btn btn-danger mx-2">Cancel</button>
-									<button type="submit" class="btn btn-success mx-2">Save</button>
-
-								</div>
-							</div>
-						</form>
 					</div>
-					<!-- // END drawer-layout__content -->
-
-					<?php include './shared/drawer.html'; ?>
 				</div>
-				<!-- // END drawer-layout -->
-			</div>
-			<!-- // END header-layout__content -->
+				<div class="form-group">
+					<label class="text-label" for="password_2">Password:</label>
+					<div class="input-group input-group-merge">
+						<input
+							id="password_2"
+							type="password"
+							required=""
+							class="form-control form-control-prepended"
+							placeholder="Enter your password"
+						/>
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								<span class="fa fa-key"></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="form-group mb-5">
+					<div class="custom-control custom-checkbox">
+						<input
+							type="checkbox"
+							class="custom-control-input"
+							checked
+							id="remember"
+						/>
+						<label class="custom-control-label" for="remember"
+							>Remember me</label
+						>
+					</div>
+				</div>
+				<div class="form-group text-center">
+					<a href="medicine-generic-name.php" class="btn btn-primary mb-5" type="submit">Login</a>
+				</div>
+			</form>
 		</div>
-		<!-- // END header-layout -->
 
-		<?php include './shared/scripts.html'; ?>
+		<!-- jQuery -->
+		<script src="assets/vendor/jquery.min.js"></script>
 
+		<!-- Bootstrap -->
+		<script src="assets/vendor/popper.min.js"></script>
+		<script src="assets/vendor/bootstrap.min.js"></script>
+
+		<!-- Perfect Scrollbar -->
+		<script src="assets/vendor/perfect-scrollbar.min.js"></script>
+
+		<!-- DOM Factory -->
+		<script src="assets/vendor/dom-factory.js"></script>
+
+		<!-- MDK -->
+		<script src="assets/vendor/material-design-kit.js"></script>
+
+		<!-- App -->
+		<script src="assets/js/toggle-check-all.js"></script>
+		<script src="assets/js/check-selected-row.js"></script>
+		<script src="assets/js/dropdown.js"></script>
+		<script src="assets/js/sidebar-mini.js"></script>
+		<script src="assets/js/app.js"></script>
+
+		<!-- App Settings (safe to remove) -->
+		<script src="assets/js/app-settings.js"></script>
 	</body>
 </html>
